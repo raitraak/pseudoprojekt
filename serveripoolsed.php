bc66a6223daf035df721ec1fb94c3aa9e55552c5
raitraak <raitraak@khk.ee>
2015-09-15 00:24:38 +0300

    #2_Stringid [Delivers #103357890];

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,18 +1,6 @@
 <?php
 
-class isik_klass {
-   var $eesnimi = 'Rait';
-   var $perenimi = 'Raak';
-    var $vanus = '25';
-    var $sugu = 'mees';
-}
-
-$isik = new isik_klass;
-
-echo $isik->eesnimi.' ';
-echo $isik->perenimi.' ';
-echo $isik->vanus.' ';
-echo $isik->sugu;
-
+echo 'Rait'.' ';
+echo 'Raak';
 
 ?>
