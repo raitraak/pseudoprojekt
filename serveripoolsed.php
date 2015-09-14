f122fc6c9b753f87cc74ad5693b77c75b5320bef
raitraak <raitraak@khk.ee>
2015-09-14 23:22:17 +0300

    1423460-103318598 #7_objektid

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,16 +1,18 @@
 <?php
 
-$eesnimi = 'Rait';
-$perekonnanimi = 'Raak';
-$vanus = 25;
-$nimi_algab_vokaaliga = False;
-
-$isik = $eesnimi.' '.$perekonnanimi.' '."($vanus)";
-
-echo $isik;
-
-if ($nimi_algab_vokaaliga == False) {
-    echo "<br>Nimi ei alga vokaaliga";
+class isik_klass {
+   var $eesnimi = 'Rait';
+   var $perenimi = 'Raak';
+    var $vanus = '25';
+    var $sugu = 'mees';
 }
+
+$isik = new isik_klass;
+
+echo $isik->eesnimi.' ';
+echo $isik->perenimi.' ';
+echo $isik->vanus.' ';
+echo $isik->sugu;
+
 
 ?>
