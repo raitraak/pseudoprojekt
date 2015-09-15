e6c257231335817b857065ccb3ab363e3208b610
raitraak <raitraak@khk.ee>
2015-09-15 15:23:57 +0300

    #9_Floats [Delivers #103402850];

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,48 +1,8 @@
 <?php
 
-$eesnimi = 'Rait';
-$perenimi = 'Raak';
-$vanus = 25;
-$isik = $eesnimi.' '.$perenimi.' '."($vanus)";
-
-echo $isik.'<br>';
-
-if ($eesnimi[0] == 'A')
-{
-
-    echo 'Nimi algab vokaaliga';
-}
-elseif ($eesnimi[0] == 'e')
-{
-
-    echo 'Nimi algab vokaaliga';
-}
-
-elseif ($eesnimi[0] == 'i')
-{
-
-    echo 'Nimi algab vokaaliga';
-}
-elseif ($eesnimi[0] == 'o')
-{
-
-    echo 'Nimi algab vokaaliga';
-}
-
-elseif ($eesnimi[0] == 'u')
-{
-
-    echo 'Nimi algab vokaaliga';
-}
-
-elseif ($eesnimi[0] == 'ü')
-{
-
-    echo 'Nimi algab vokaaliga';
-}
-
-else {
-    echo 'Nimi ei alga vokaaliga';
-}
+$x = 1.23;
+$y = 2.34;
+$vastus = $x + $y;
+echo $vastus
 
 ?>
