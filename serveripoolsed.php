6cbd23418394b983e49fd9bc866a9d1098f37e82
raitraak <raitraak@khk.ee>
2015-09-15 13:48:59 +0300

    #6_Tingimused [Delivers #103398310];

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -4,10 +4,14 @@ $eesnimi = 'Rait';
 $perenimi = 'Raak';
 $vanus = '25';
 $sugu = 'mees';
+$nimi_algab_vokaaliga = False;
 
 $isik = $eesnimi.' '.$perenimi.' '."($vanus)";
 
-echo $isik
+echo $isik.'<br>';
 
+if ($nimi_algab_vokaaliga == False)
+
+    echo 'Nimi ei alga vokaaliga';
 
 ?>
