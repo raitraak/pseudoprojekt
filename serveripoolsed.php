3ba4eb4565c5b26fa72d19b98b6752cad1e7d3f3
raitraak <raitraak@khk.ee>
2015-09-15 19:16:21 +0300

    #11_$Post [Delivers #103417594];

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,8 +1,28 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
 
-echo 'Koer: '.($_GET["koer"]);
+<form action="?" method="POST">
+    <input name="kass">
+    <input type='submit'>
+</form>
+
+<?php
+$kass1 = $_POST['kass'];
+
+if ($kass1['kass'])
+{
+    echo 'Kass oli: '.$_POST['kass'];
+}
+
 
 ?>
 
+</body>
+</html>
 
 
