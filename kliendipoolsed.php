c69df516f34676b382e45de7c9255046f8600375
raitraak <raitraak@khk.ee>
2015-09-21 02:29:04 +0300

    #6_Jquery kass koeraks_kliendipoolsed [Delivers #103754400];

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
 
 <img src="http://t2.gstatic.com/images?q=tbn:ANd9GcTUDQgbT9PFaERFaLbqP8sFsyq2r3sSYu6BtCj63z90tLEpALgo" id="kass" onclick="koeraks()"/>
 
-<script type="text/javascript">
+<script>
 
-    function koeraks() {
-
-        document.getElementById('kass').src = 'http://animalia-life.com/data_images/dog/dog1.jpg';
-    }
+    $('#kass').click(function koeraks(){
+        $(this).attr('src','http://animalia-life.com/data_images/dog/dog7.jpg');
+    });
 
 </script>
 
