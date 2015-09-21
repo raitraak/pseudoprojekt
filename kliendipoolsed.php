83ef84c2fc4bdd70330def1be10a6bab8576c2ba
raitraak <raitraak@khk.ee>
2015-09-22 00:42:22 +0300

    #6_Jquery kass koeraks_kliendipoolsed [Delivers #103754400];

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 </head>
 <body>
 
 <img src="pildid/kass-66103366.jpg" id="kass" onclick="koeraks()">
 
 <script>
-    function koeraks() {
+    $('#kass').click(function(){
 
-        document.getElementById('kass').src='pildid/kutsikas-koer-67073776.jpg';
-    }
+        $(this).attr('src','pildid/kutsikas-koer-67073776.jpg');
+    });
 
 </script>
 </body>
