8e7ea04073e94a42bae0943a64d176ea958f2a49
raitraak <raitraak@khk.ee>
2015-09-21 23:19:16 +0300

    #7_ Jquery taustavärvi muutmine_kliendipoolsed [Delivers #103754966];

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,23 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
 
-<button type="submit" onclick="punaseks()">RED</button>
-<button type="submit" onclick="roheliseks()">GREEN</button>
-<button type="submit" onclick="siniseks()">BLUE</button>
+<input type="button" onclick="tere()" value="Tere Maailm">
 
 <script>
-    function punaseks() {
-        $('body').css('background','red');
-    }
-    function roheliseks() {
-        $('body').css('background','green');
-    }
-    function siniseks() {
-        $('body').css('background','blue');
+    function tere() {
+        alert('Tere Maailm!')
     }
 </script>
 </body>
