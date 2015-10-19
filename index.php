fd161a4a850805d6fb8829f677cf55f5cb77bf87
raitraak <raitraak@khk.ee>
2015-10-20 02:07:29 +0300

    31_#andmebaasist andmete võtmine [Delivers #106053172];

--- index.php
+++ index.php
@@ -114,19 +114,19 @@ if(mysqli_num_rows($result)>0) {
 
     <!-- Main Content -->
     <div class="container">
-        <?php foreach ($posts as $post): ?>
+        <?php foreach ($posts as $post):?>
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-preview">
                     <a href="post.html">
                         <h2 class="post-title">
-                            <?php $post['title'] ?>
+                            <?=$post['title'] ?>
                         </h2>
                         <h3 class="post-subtitle">
-                            <?php $post['description'] ?>
+                            <?=$post['description'] ?>
                         </h3>
                     </a>
-                    <p class="post-meta">Posted by <a href="#"><?php $post['name'] ?></a> on <?php $post['date'] ?></p>
+                    <p class="post-meta">Posted by <a href="#"><?=$post['name'] ?></a> on <?=$post['date'] ?></p>
                 </div>
                 <hr>
 
